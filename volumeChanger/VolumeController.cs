20c16ec02cf83ed143577519151a8daedbdb22c9
larswarnken <lars.warnken@protonmail.com>
2024-10-22 15:42:29 +0200

    controlling program volumes

--- volumeChanger/VolumeController.cs
+++ volumeChanger/VolumeController.cs
@@ -4,27 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NAudio.CoreAudioApi;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System;
+using System.Linq;
 
 namespace volumeChanger
 {
     internal class VolumeController
     {
         int[] _pinValues = new int[5];
-        int[] _previousValues = [0, 0, 0, 0, 0];
+        int[] _previousValues = new int[5];
 
-        public event EventHandler<PinValueEventArgs> PinValueChanged;
+
+        // Erstelle ein festes Array mit der Größe 4, das 4 Listen von Strings enthält
+        List<string>[] programArray = new List<string>[4];
+
+
+
+        public event EventHandler<PinValueEventArgs>? PinValueChanged;
 
         public VolumeController()
         {
+            PinValueChanged = null;
+
+            // Initialisiere jede Liste in dem Array
+            for (int i = 0; i < programArray.Length; i++)
+            {
+                programArray[i] = new List<string>();
+            }
+
+            // Füge Werte zu den Listen hinzu
+            programArray[0].Add("brave");
+            programArray[0].Add("chrome");
+            programArray[1].Add("Spotify");
+            programArray[1].Add("spotify");
+            programArray[2].Add("discord");
+            programArray[3].Add("valorant");
+
         }
 
         // Method to raise the event
         protected virtual void OnPinValueChanged(int pinValue)
         {
             PinValueChanged?.Invoke(this, new PinValueEventArgs(pinValue));
-            Console.WriteLine("event raised");
         }
 
+        // speichert die einzelnen Werte in dem pin values array
         public void SplitData(string data)
         {
             string[] parts = data.Split('|');
@@ -37,9 +63,10 @@ namespace volumeChanger
         // Method to process the volume data
         public void ProcessVolumeData(string data)
         {
+            // split data into individual values
             SplitData(data);
-            Console.WriteLine(_pinValues[0]);
 
+            // general volume
             if (_pinValues[0] != _previousValues[0])
             {
                 SetGeneralVolume(_pinValues[0]);
@@ -48,13 +75,30 @@ namespace volumeChanger
                 OnPinValueChanged(_pinValues[0]);
             }
 
+            // process volume
+            for (int i = 1; i < 5; i++)
+            {
+                if (_pinValues[i] != _previousValues[i])
+                {
+                    foreach (var program in programArray[i - 1])
+                    {
+                        SetProcessVolume(program, _pinValues[i]);
+
+                        _previousValues[i] = _pinValues[i];
+                    }
+                }
+            }
+
+
         }
 
+        // Method to get the pin values
         public int[] GetPinValues()
         {
             return _pinValues;
         }
 
+        // Method to set the general volume
         public void SetGeneralVolume(int volumeLevel)
         {
             if (volumeLevel < 0 || volumeLevel > 100)
@@ -67,6 +111,48 @@ namespace volumeChanger
             var deviceEnumerator = new MMDeviceEnumerator();
             var defaultDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
             defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar = volumeScalar;
+        }
+
+        // Method to set the volume of a specific program
+        public void SetProcessVolume(string processName, int volume)
+        {
+            if (volume < 0 || volume > 100)
+                throw new ArgumentOutOfRangeException(nameof(volume), "Lautstärke muss zwischen 0 und 100 liegen.");
+
+            // Verwende den Audio-Endpunkt
+            var deviceEnumerator = new MMDeviceEnumerator();
+            var device = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+            // Suche nach allen aktiven Audio-Sessions
+            var sessions = device.AudioSessionManager.Sessions;
+
+
+            // Iteriere über alle Sessions, um den Prozess zu finden
+            for (int i = 0; i < sessions.Count; i++)
+            {
+                var session = sessions[i];
+                var processId = session.GetProcessID;
+
+
+                try
+                {
+                    var process = Process.GetProcessById((int)processId);
+
+                    // Überprüfen, ob der Prozessname übereinstimmt
+                    if (process.ProcessName.Equals(processName, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        // Setze die Lautstärke (zwischen 0.0 und 1.0)
+                        session.SimpleAudioVolume.Volume = volume / 100f;
+                        Console.WriteLine($"Setze Lautstärke für {processName} (PID: {process.Id}) auf {volume}%.");
+                        return;
+                    }
+                }
+                catch (ArgumentException)
+                {
+                    // Ignoriere Prozesse, die nicht mehr existieren
+                    continue;
+                }
+            }
         }
     }
 }
