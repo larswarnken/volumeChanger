4732ac03355f807d915ce716d0e853f83e79268d
larswarnken <lars.warnken@protonmail.com>
2024-10-23 20:38:48 +0200

    added json saving and loading

--- volumeChanger/Form2.cs
+++ volumeChanger/Form2.cs
@@ -7,13 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace volumeChanger
 {
     public partial class Form2 : Form
     {
-        private RichTextBox lastFocusedTextBox;
+        private RichTextBox? lastFocusedTextBox;
+
+        private SettingManager? settingsManager;
 
         public Form2()
         {
@@ -28,9 +31,13 @@ namespace volumeChanger
             richTextBox2.Enter += textBox_Enter;
             richTextBox3.Enter += textBox_Enter;
             richTextBox4.Enter += textBox_Enter;
+
+            settingsManager = new SettingManager();
+
+            LoadSettings();
         }
 
-        private void textBox_Enter(object sender, EventArgs e)
+        private void textBox_Enter(object? sender, EventArgs e)
         {
             lastFocusedTextBox = sender as RichTextBox ?? throw new ArgumentNullException(nameof(sender));
         }
@@ -38,9 +45,31 @@ namespace volumeChanger
         private void buttonSave_Click(object sender, EventArgs e)
         {
             // Perform button click actions here
+            settingsManager?.SaveSettings(richTextBox1, richTextBox2, richTextBox3, richTextBox4);
 
             // Set focus back to the text box
             lastFocusedTextBox?.Focus();
+        }
+
+        public void LoadSettings()
+        {
+            string exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(exeDirectory, "settings.json");
+
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                var content = JsonConvert.DeserializeObject<dynamic>(json);
+
+                richTextBox1.Text = content.Pin1;
+                richTextBox2.Text = content.Pin2;
+                richTextBox3.Text = content.Pin3;
+                richTextBox4.Text = content.Pin4;
+            }
+            else
+            {
+                MessageBox.Show("Settings file not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
--- volumeChanger/SettingManager.cs
+++ volumeChanger/SettingManager.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Windows.Forms;
 
 namespace volumeChanger
 {
     internal class SettingManager
     {
+        public SettingManager()
+        {
+            // Constructor logic here
+        }
+
+        public void SaveSettings(RichTextBox rtb1, RichTextBox rtb2, RichTextBox rtb3, RichTextBox rtb4)
+        {
+            var content = new
+            {
+                Pin1 = rtb1.Text,
+                Pin2 = rtb2.Text,
+                Pin3 = rtb3.Text,
+                Pin4 = rtb4.Text
+            };
+
+            string json = JsonConvert.SerializeObject(content, Newtonsoft.Json.Formatting.Indented);
+            string exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(exeDirectory, "settings.json");
+
+            File.WriteAllText(filePath, json);
+        }
     }
 }
