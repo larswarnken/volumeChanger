5800b4b4c9379eea47bda151af97fbadefe76309
larswarnken <lars.warnken@protonmail.com>
2024-10-23 19:02:17 +0200

    deleted gui manager + added save button

--- volumeChanger/Form2.Designer.cs
+++ volumeChanger/Form2.Designer.cs
@@ -46,6 +46,8 @@
             panelBorder2 = new Panel();
             panelpadding4 = new Panel();
             richTextBox4 = new RichTextBox();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            buttonSave = new Button();
             tableLayoutPanel1.SuspendLayout();
             panelBorder4.SuspendLayout();
             panelPadding3.SuspendLayout();
@@ -59,6 +61,7 @@
             panelPadding1.SuspendLayout();
             panelBorder2.SuspendLayout();
             panelpadding4.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -79,12 +82,12 @@
             tableLayoutPanel1.Controls.Add(pictureBoxListIcon4, 3, 0);
             tableLayoutPanel1.Controls.Add(panelBorder1, 0, 1);
             tableLayoutPanel1.Controls.Add(panelBorder2, 1, 1);
-            tableLayoutPanel1.Location = new Point(34, 38);
+            tableLayoutPanel1.Location = new Point(3, 3);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 2;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 80F));
-            tableLayoutPanel1.Size = new Size(752, 400);
+            tableLayoutPanel1.Size = new Size(770, 377);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // panelBorder4
@@ -92,9 +95,9 @@
             panelBorder4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             panelBorder4.BackColor = SystemColors.ControlDark;
             panelBorder4.Controls.Add(panelPadding3);
-            panelBorder4.Location = new Point(379, 83);
+            panelBorder4.Location = new Point(387, 78);
             panelBorder4.Name = "panelBorder4";
-            panelBorder4.Size = new Size(182, 314);
+            panelBorder4.Size = new Size(186, 296);
             panelBorder4.TabIndex = 2;
             // 
             // panelPadding3
@@ -105,7 +108,7 @@
             panelPadding3.Location = new Point(2, 2);
             panelPadding3.Name = "panelPadding3";
             panelPadding3.Padding = new Padding(2);
-            panelPadding3.Size = new Size(178, 310);
+            panelPadding3.Size = new Size(182, 292);
             panelPadding3.TabIndex = 0;
             // 
             // richTextBox3
@@ -117,7 +120,7 @@
             richTextBox3.ForeColor = SystemColors.ControlDark;
             richTextBox3.Location = new Point(4, 4);
             richTextBox3.Name = "richTextBox3";
-            richTextBox3.Size = new Size(170, 302);
+            richTextBox3.Size = new Size(174, 284);
             richTextBox3.TabIndex = 0;
             richTextBox3.Text = "";
             // 
@@ -126,9 +129,9 @@
             panelBorder3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             panelBorder3.BackColor = SystemColors.ControlDark;
             panelBorder3.Controls.Add(panelPadding2);
-            panelBorder3.Location = new Point(191, 83);
+            panelBorder3.Location = new Point(195, 78);
             panelBorder3.Name = "panelBorder3";
-            panelBorder3.Size = new Size(182, 314);
+            panelBorder3.Size = new Size(186, 296);
             panelBorder3.TabIndex = 1;
             // 
             // panelPadding2
@@ -139,7 +142,7 @@
             panelPadding2.Location = new Point(2, 2);
             panelPadding2.Name = "panelPadding2";
             panelPadding2.Padding = new Padding(2);
-            panelPadding2.Size = new Size(178, 310);
+            panelPadding2.Size = new Size(182, 292);
             panelPadding2.TabIndex = 0;
             // 
             // richTextBox2
@@ -151,14 +154,14 @@
             richTextBox2.ForeColor = SystemColors.ControlDark;
             richTextBox2.Location = new Point(4, 4);
             richTextBox2.Name = "richTextBox2";
-            richTextBox2.Size = new Size(170, 302);
+            richTextBox2.Size = new Size(174, 284);
             richTextBox2.TabIndex = 0;
             richTextBox2.Text = "";
             // 
             // pictureBoxListIcon1
             // 
             pictureBoxListIcon1.Anchor = AnchorStyles.Top;
-            pictureBoxListIcon1.Location = new Point(62, 3);
+            pictureBoxListIcon1.Location = new Point(64, 3);
             pictureBoxListIcon1.Name = "pictureBoxListIcon1";
             pictureBoxListIcon1.Size = new Size(64, 64);
             pictureBoxListIcon1.TabIndex = 0;
@@ -167,7 +170,7 @@
             // pictureBoxListIcon2
             // 
             pictureBoxListIcon2.Anchor = AnchorStyles.Top;
-            pictureBoxListIcon2.Location = new Point(250, 3);
+            pictureBoxListIcon2.Location = new Point(256, 3);
             pictureBoxListIcon2.Name = "pictureBoxListIcon2";
             pictureBoxListIcon2.Size = new Size(64, 64);
             pictureBoxListIcon2.TabIndex = 1;
@@ -176,7 +179,7 @@
             // pictureBoxListIcon3
             // 
             pictureBoxListIcon3.Anchor = AnchorStyles.Top;
-            pictureBoxListIcon3.Location = new Point(438, 3);
+            pictureBoxListIcon3.Location = new Point(448, 3);
             pictureBoxListIcon3.Name = "pictureBoxListIcon3";
             pictureBoxListIcon3.Size = new Size(64, 64);
             pictureBoxListIcon3.TabIndex = 2;
@@ -185,7 +188,7 @@
             // pictureBoxListIcon4
             // 
             pictureBoxListIcon4.Anchor = AnchorStyles.Top;
-            pictureBoxListIcon4.Location = new Point(626, 3);
+            pictureBoxListIcon4.Location = new Point(641, 3);
             pictureBoxListIcon4.Name = "pictureBoxListIcon4";
             pictureBoxListIcon4.Size = new Size(64, 64);
             pictureBoxListIcon4.TabIndex = 3;
@@ -196,9 +199,9 @@
             panelBorder1.BackColor = SystemColors.ControlDark;
             panelBorder1.Controls.Add(panelPadding1);
             panelBorder1.Dock = DockStyle.Fill;
-            panelBorder1.Location = new Point(3, 83);
+            panelBorder1.Location = new Point(3, 78);
             panelBorder1.Name = "panelBorder1";
-            panelBorder1.Size = new Size(182, 314);
+            panelBorder1.Size = new Size(186, 296);
             panelBorder1.TabIndex = 0;
             // 
             // panelPadding1
@@ -209,7 +212,7 @@
             panelPadding1.Location = new Point(2, 2);
             panelPadding1.Name = "panelPadding1";
             panelPadding1.Padding = new Padding(2);
-            panelPadding1.Size = new Size(178, 310);
+            panelPadding1.Size = new Size(182, 292);
             panelPadding1.TabIndex = 0;
             // 
             // richTextBox1
@@ -221,7 +224,7 @@
             richTextBox1.ForeColor = SystemColors.ControlDark;
             richTextBox1.Location = new Point(4, 4);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(170, 302);
+            richTextBox1.Size = new Size(174, 284);
             richTextBox1.TabIndex = 0;
             richTextBox1.Text = "";
             // 
@@ -230,9 +233,9 @@
             panelBorder2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             panelBorder2.BackColor = SystemColors.ControlDark;
             panelBorder2.Controls.Add(panelpadding4);
-            panelBorder2.Location = new Point(567, 83);
+            panelBorder2.Location = new Point(579, 78);
             panelBorder2.Name = "panelBorder2";
-            panelBorder2.Size = new Size(182, 314);
+            panelBorder2.Size = new Size(188, 296);
             panelBorder2.TabIndex = 3;
             // 
             // panelpadding4
@@ -243,7 +246,7 @@
             panelpadding4.Location = new Point(2, 2);
             panelpadding4.Name = "panelpadding4";
             panelpadding4.Padding = new Padding(2);
-            panelpadding4.Size = new Size(178, 310);
+            panelpadding4.Size = new Size(184, 292);
             panelpadding4.TabIndex = 0;
             // 
             // richTextBox4
@@ -255,16 +258,49 @@
             richTextBox4.ForeColor = SystemColors.ControlDark;
             richTextBox4.Location = new Point(4, 4);
             richTextBox4.Name = "richTextBox4";
-            richTextBox4.Size = new Size(170, 302);
+            richTextBox4.Size = new Size(176, 284);
             richTextBox4.TabIndex = 0;
             richTextBox4.Text = "";
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel2.ColumnCount = 1;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.Controls.Add(buttonSave, 0, 1);
+            tableLayoutPanel2.Controls.Add(tableLayoutPanel1, 0, 0);
+            tableLayoutPanel2.Location = new Point(12, 12);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 2;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 90F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel2.Size = new Size(776, 426);
+            tableLayoutPanel2.TabIndex = 1;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Anchor = AnchorStyles.None;
+            buttonSave.BackColor = SystemColors.Control;
+            buttonSave.Cursor = Cursors.Hand;
+            buttonSave.FlatAppearance.BorderSize = 2;
+            buttonSave.FlatStyle = FlatStyle.Flat;
+            buttonSave.Font = new Font("Cabin", 9.749999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            buttonSave.ForeColor = SystemColors.ControlDark;
+            buttonSave.Location = new Point(348, 389);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(80, 30);
+            buttonSave.TabIndex = 2;
+            buttonSave.TabStop = false;
+            buttonSave.Text = "Speichern";
+            buttonSave.UseVisualStyleBackColor = false;
+            buttonSave.Click += buttonSave_Click;
             // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(tableLayoutPanel1);
+            Controls.Add(tableLayoutPanel2);
             Icon = (Icon)resources.GetObject("$this.Icon");
             MinimizeBox = false;
             Name = "Form2";
@@ -282,8 +318,9 @@
             panelPadding1.ResumeLayout(false);
             panelBorder2.ResumeLayout(false);
             panelpadding4.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -305,5 +342,7 @@
         private RichTextBox richTextBox3;
         private RichTextBox richTextBox2;
         private RichTextBox richTextBox4;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Button buttonSave;
     }
 }
--- volumeChanger/Form2.cs
+++ volumeChanger/Form2.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace volumeChanger
 {
     public partial class Form2 : Form
     {
+        private RichTextBox lastFocusedTextBox;
+
         public Form2()
         {
             InitializeComponent();
@@ -20,6 +23,24 @@ namespace volumeChanger
             pictureBoxListIcon2.Image = Image.FromFile("icons/spotify.png");
             pictureBoxListIcon3.Image = Image.FromFile("icons/discord.png");
             pictureBoxListIcon4.Image = Image.FromFile("icons/gaming.png");
+
+            richTextBox1.Enter += textBox_Enter;
+            richTextBox2.Enter += textBox_Enter;
+            richTextBox3.Enter += textBox_Enter;
+            richTextBox4.Enter += textBox_Enter;
+        }
+
+        private void textBox_Enter(object sender, EventArgs e)
+        {
+            lastFocusedTextBox = sender as RichTextBox ?? throw new ArgumentNullException(nameof(sender));
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            // Perform button click actions here
+
+            // Set focus back to the text box
+            lastFocusedTextBox?.Focus();
         }
     }
 }
